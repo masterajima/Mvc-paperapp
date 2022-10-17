4124562c21d5d30db184c6e60521cd44b335b86e
Ajima Master <98704309+masterajima@users.noreply.github.com>
2022-10-17 16:45:45 -0400

    data

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MvcZipperPaper.Models;
+
+namespace MvcZipperPaper.Data
+{
+    public class MvcZipperPaperContext : IdentityDbContext
+    {
+        public MvcZipperPaperContext(DbContextOptions<MvcZipperPaperContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MvcZipperPaper.Models.Zipper> Zipper { get; set; }
+    }
+}
